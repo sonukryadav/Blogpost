ea6a5ec6e95cdca7a3b3e41137b230b37fd74cd3
sonukryadav <sonukryadav56@gmail.com>
2023-02-11 20:45:14 +0530

    posts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -5,17 +5,17 @@ import auth from '../middleware/auth.js'
 const router = express.Router()
 
 // http://localhost:5000/posts/
-router.get('/', auth, getPosts)
-router.get('/search', auth, getPostsBySearch)
-router.get('/:id', auth, getPost)
+router.get('/', auth, getPosts);
+router.get('/search', auth, getPostsBySearch);
+router.get('/:id', auth, getPost);
 
-router.post('/', auth, createPost)
-router.post('/:id/commentPost', auth, commentPost)
+router.post('/', auth, createPost);
+router.post('/:id/commentPost', auth, commentPost);
 
-router.delete('/:id', auth, deletePost)
+router.delete('/:id', auth, deletePost);
 
-router.patch('/:id/deleteComment', deleteComment)
-router.patch('/:id', auth, updatePost)
-router.patch('/:id/likePost', auth, likePost)
+router.patch('/:id/deleteComment', deleteComment);
+router.patch('/:id', auth, updatePost);
+router.patch('/:id/likePost', auth, likePost);
 
-export default router
+export default router;
