2ee05d6a68f42496ee22e18b76c36e00bb37634f
sonukryadav <sonukryadav56@gmail.com>
2023-02-14 23:18:23 +0530

    auth

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,14 +1,14 @@
-import { AUTH, LOGOUT } from "../constants/actionTypes"
+import { AUTH, LOGOUT } from "../constants/actionTypes";
 
 export default (state = { authData: null }, action) => {
     switch (action.type) {
         case AUTH:
             localStorage.setItem("profile", JSON.stringify({ ...action?.data }))
-            return { ...state, authData: action?.data }
+            return { ...state, authData: action?.data };
         case LOGOUT:
             localStorage.clear()
-            return { ...state, authData: null }
+            return { ...state, authData: null };
         default:
-            return state
+            return state;
     }
 }
